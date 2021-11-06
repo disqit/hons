199f4a1b4ddebdfe42ba82037f72314740ebf95a
Jason <bothajason2@gmail.com>
2021-11-06 16:43:30 +0200

    Create open_test_sites.bat
    Added batch file for automated sample sites loading

---
+++ open_test_sites.bat
@@ -0,0 +1,23 @@
+@echo off
+start chrome https://cybersecuritylog.com/japans-aims-to-make-its-cybersecurity-more-powerful
+start chrome https://iq.opengenus.org/2d-array-in-numpy/
+start chrome https://learn2021.ukzn.ac.za/
+start chrome https://ostoday.org/linux/which-linux-distro-should-i-use-quiz.html
+start chrome https://seriot.ch/projects/parsing_json.html
+start chrome https://shop.fairphone.com/
+start chrome https://stattrek.com/online-calculator/bayes-rule-calculator.aspx
+start chrome https://therecipecritic.com/quick-easy-dough/
+start chrome https://towardsai.net/p/programming/decision-trees-explained-with-a-practical-example-fe47872d3b53
+start chrome https://trendscatchers.co.uk/index.php/en/2021/03/17/prince-williams-old-university-flame-reignites-cheating-rumours-2/
+start chrome https://www.businessinsider.com/ireland-corporation-tax-facebook-google-apple-15-minimum-rate-2021-10?IR=T
+start chrome https://www.cnbc.com/2021/10/10/southwest-airlines-cancels-1000-more-flights-as-disruptions-mount.html
+start chrome https://www.dailymail.co.uk/news/article-10073543/Shutdown-winter-UK-Steel-says-rocketing-energy-costs-strangle-production.html
+start chrome https://www.enca.com/news/oecd-hails-major-victory-global-tax-holdouts-join-reform
+start chrome https://www.gameinformer.com/review/far-cry-6/far-cry-6-review-the-spark-that-ignites-the-fire
+start chrome https://www.gamesradar.com/uk/harry-potter-director-reveals-robin-williams-wanted-to-play-lupin/
+start chrome https://www.gsmarena.com/samsung_says_that_ray_tracing_is_coming_to_mobile_games_with_the_exynos_2200-news-51255.php
+start chrome https://www.insider.com/australia-koalas-vaccinated-against-chlamydia-2021-10
+start chrome https://www.nola.com/news/crime_police/article_4a2a61d2-2c29-11ec-8d09-6f5e1d856870.html
+start chrome https://www.theguardian.com/world/2021/oct/11/rotting-red-sea-oil-tanker-could-leave-8m-people-without-water
+start chrome https://www.the-scientist.com/news-opinion/scientists-use-photosynthesis-to-power-an-animal-s-brain-69307
+start chrome https://www.theverge.com/2021/10/18/22726444/apple-m1-pro-max-chip-processor-arm-macbook-fastest
